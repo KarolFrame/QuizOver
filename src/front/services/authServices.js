631ac6aa947da40542213956bd4bf1c0234d684e
Genesis Iabichino <geniabichino@gmail.com>
2025-06-24 14:36:10 +0200

    fix login endpoint

--- src/front/services/authServices.js
+++ src/front/services/authServices.js
@@ -1,18 +1,21 @@
 export const login = async (username, password) => {
-  const BACKURL = `https://ubiquitous-sniffle-jprwv559r64hpq75-3001.app.github.dev`;
-  const resp = await fetch(BACKURL, {
+  const BACKURL = import.meta.env.VITE_BACKEND_URL;
+  const resp = await fetch(`${BACKURL}/token`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password }),
   });
 
-  if (!resp.ok) throw Error("There was a problem in the login request");
-
   if (resp.status === 401) {
+    const data = await resp.json();
+    alert(data.msg)
     throw "Invalid credentials";
   } else if (resp.status === 400) {
     throw "Invalid email or password format";
+  } else if (!resp.ok) {
+    throw Error("There was a problem in the login request");
   }
+  
   const data = await resp.json();
   // Save your token in the localStorage
   // Also you should set your user into the store using the setItem function
--- src/front/services/taskService.js
+++ src/front/services/taskService.js
@@ -1,7 +1,6 @@
 // Assuming "/protected" is a private endpoint
 export const getMyTasks = async () => {
-  export const login = async (username, password) => {
-  const BACKURL = `https://ubiquitous-sniffle-jprwv559r64hpq75-3001.app.github.dev/protected`;
+  const BACKURL = `${import.meta.env.VITE_BACKEND_URL}/protected`;
   // Retrieve token from localStorage
   const token = localStorage.getItem("jwt-token");
 
@@ -9,18 +8,13 @@ export const getMyTasks = async () => {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: "Bearer " + token, // ⬅⬅⬅ authorization token
+      "Authorization": "Bearer " + token, // ⬅⬅⬅ authorization token
     },
   });
 
   if (!resp.ok) {
     throw Error("There was a problem in the login request");
-  } else if (resp.status === 403) {
-    throw Error("Missing or invalid token");
-  } else {
-    throw Error("Unknown error");
-  }
-
+  } 
   const data = await resp.json();
   console.log("This is the data you requested", data);
   return data;
