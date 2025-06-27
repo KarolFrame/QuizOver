0a306be1a509779b6d8535519db40c3d3259aea7
Carolina <97622225+KarolFrame@users.noreply.github.com>
2025-06-27 23:33:43 +0000

    finish register and implement favico ant title

--- src/front/components/RegisterForm.jsx
+++ src/front/components/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import { login } from '../services/authServices.js';
+import { register } from '../services/authServices.js';
 import { useState } from 'react';
 import { Button } from './Button.jsx';
 
@@ -7,24 +7,46 @@ export const RegisterForm = () => {
     const [password, setPassword] = useState('');
 
     const handleSubmit = async (event) => {
-        event.preventDefault()
+        event.preventDefault();
         try {
-            const data = await login(email, password);
-            console.log("login:", data)
+            const data = await register(email, password);
+            console.log("register:", data);
+            if (data.token) {
+                localStorage.setItem("jwt-token", data.token);
+            }
+            localStorage.setItem("user-email", email);
+            alert("Registration successful!");
+            setEmail('');
+            setPassword('');
+
+            // redirigir el registro, programar aqui
+
         } catch (error) {
-            console.error("error login:", error.message);
+            console.error("error register:", error.message);
         }
-    }
+    };
 
     return (
         <>
             <div className='flex justify-center'>
                 <form className='flex flex-col bg-primary' onSubmit={handleSubmit}>
-                    <input type="email" placeholder='Type your email' required value={email} onChange={event => setEmail(event.target.value)} />
-                    <input type="password" placeholder='Create a password' required value={password} onChange={event => setPassword(event.target.value)} />
-                    <Button label="Create Account" variant="accent" />
+                    <input
+                        type="email"
+                        placeholder='Type your email'
+                        required
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder='Create a password'
+                        required
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                    />
+                    <Button label="Create Account" variant="accent" type='submit' />
                 </form>
             </div>
         </>
-    )
+    );
 };
