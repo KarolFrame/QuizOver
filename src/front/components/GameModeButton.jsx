9e54af2264640684c9847368a6ef6dce0ba10c88
Antonino Morana <amorana1508@gmail.com>
2025-07-03 09:06:33 +0000

    Avance Botones

--- src/front/components/GameModeButton.jsx
+++ src/front/components/GameModeButton.jsx
@@ -11,7 +11,7 @@ export const GameModeButton = ({ label, onClick, icon = null, className = "" }) 
 
   return (
     <button onClick={onClick} className={finalClass}>
-      {icon && <img src={icon} alt={`${label} icon`} className="w-8 h-8 object-contain" />}
+      {icon && <img src={icon} alt={`${label} icon`} className="w-13 h-13 object-contain" />}
       <span className="leading-snug">{label}</span>
     </button>
   );
