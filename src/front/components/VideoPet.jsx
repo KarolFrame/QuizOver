437ee9f468eb57f08f6d4b7f8d13521ae9618f72
Genesis Iabichino <geniabichino@gmail.com>
2025-07-04 23:30:53 +0200

    New home (WIP)

--- src/front/components/VideoPet.jsx
+++ src/front/components/VideoPet.jsx
@@ -6,35 +6,35 @@ export const VideoPet = () => {
 
     return (
         <>
-            {!isIdle?(
-            <video
-                className="mx-auto"
-                src="/video/v_hello.webm"
-                autoPlay
-                muted
-                playsInline
-                style={{
-                    width: "50%",
-                    height: "auto",
-                    pointerEvents: "none",
-                    background: "none"
-                }}
-                onEnded={() => setIsIdle(true)}
-            />) :(
-            <video
-                className="mx-auto"
-                src="/video/v_idle.webm"
-                autoPlay
-                loop
-                muted
-                playsInline
-                style={{
-                    width: "50%",
-                    height: "auto",
-                    pointerEvents: "none",
-                    background: "none"
-                }}
-            />)}
+            {!isIdle ? (
+                <video
+                    className="mx-auto"
+                    src="/video/v_hello.webm"
+                    autoPlay
+                    muted
+                    playsInline
+                    style={{
+                        width: "50%",
+                        height: "auto",
+                        pointerEvents: "none",
+                        background: "none"
+                    }}
+                    onEnded={() => setIsIdle(true)}
+                />) : (
+                <video
+                    className="mx-auto"
+                    src="/video/v_idle.webm"
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    style={{
+                        width: "20%",
+                        height: "auto",
+                        pointerEvents: "none",
+                        background: "none"
+                    }}
+                />)}
         </>
     )
 }
