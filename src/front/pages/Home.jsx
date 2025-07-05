24b49cdf0d0a42cf333c2c76cfddd9465be0263a
Genesis Iabichino <geniabichino@gmail.com>
2025-07-05 10:54:54 +0200

    Add new home with video and navbar

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -2,69 +2,51 @@ import React, { useEffect } from "react"
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import { Link } from "react-router-dom";
 import { VideoPet } from "../components/VideoPet.jsx";
-// import Spriteanimator from "../components/SpriteAnimator.jsx"; // Importación comentada
+import { Button } from "../components/Button.jsx";
+import { LoopingRewindVideo, HeaderVideo } from "../components/HeaderVideo.jsx";
+
 
 export const Home = () => {
 
-  const { store, dispatch } = useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
 
-  const loadMessage = async () => {
-    try {
-      const backendUrl = import.meta.env.VITE_BACKEND_URL
+	const loadMessage = async () => {
+		try {
+			const backendUrl = import.meta.env.VITE_BACKEND_URL
 
-      if (!backendUrl) throw new Error("VITE_BACKEND_URL is not defined in .env file")
+			if (!backendUrl) throw new Error("VITE_BACKEND_URL is not defined in .env file")
 
-      const response = await fetch(backendUrl + "/api/hello")
-      const data = await response.json()
+			const response = await fetch(backendUrl + "/api/hello")
+			const data = await response.json()
 
-      if (response.ok) dispatch({ type: "set_hello", payload: data.message })
+			if (response.ok) dispatch({ type: "set_hello", payload: data.message })
 
-      return data
+			return data
 
-    } catch (error) {
-      if (error.message) throw new Error(
-        `Could not fetch the message from the backend.
-        Please check if the backend is running and the backend port is public.`
-      );
-    }
-  }
+		} catch (error) {
+			if (error.message) throw new Error(
+				`Could not fetch the message from the backend.
+				Please check if the backend is running and the backend port is public.`
+			);
+		}
 
-  useEffect(() => {
-    loadMessage()
-  }, [])
+	}
 
-  return (
-    <div className="text-center mt-5">
-      <VideoPet />
-      {/* <Spriteanimator /> */} {/* Componente comentado */}
+	useEffect(() => {
+		loadMessage()
+	}, [])
 
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
-      <Link to="/register">
-        <button>Register</button>
-      </Link>
-      <Link to="/game_selector">
-        <button>Game Selector</button>
-      </Link>
-      <Link to="/about-us">
-        <button>About Us</button>
-      </Link>
-      <Link to="/game/clasic-mode">
-        <button>Classic Game</button>
-      </Link>
-      <Link to="/ranking/global">
-        <button>GlobalRanking</button>
-      </Link>
-      <div className="alert alert-info">
-        {store.message ? (
-          <span>{store.message}</span>
-        ) : (
-          <span className="text-danger">
-            Loading message from the backend (make sure your python 🐍 backend is running)...
-          </span>
-        )}
-      </div>
-    </div>
-  );
-};
+	return (
+
+		<>
+			<div className=" d-flex flex-column gap-0 text-center">
+				{/* <VideoPet /> */}
+
+				<div className="flex justify-center h-300">
+					<LoopingRewindVideo videoSrc="/video/header_video2.mp4" />
+				</div>
+			</div>
+
+		</>
+	);
+}; 
