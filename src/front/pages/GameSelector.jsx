6f24fc45afeb0e128d9e6314e5e803cccdd89376
Carolina <97622225+KarolFrame@users.noreply.github.com>
2025-07-07 14:20:10 +0000

    classic mode img

--- src/front/pages/GameSelector.jsx
+++ src/front/pages/GameSelector.jsx
@@ -1,5 +1,7 @@
 import { useNavigate, Link } from "react-router-dom";
 import { GameModeButton } from "../components/GameModeButton";
+import { motion } from "motion/react";
+import { VideoPet } from "../components/VideoPet";
 
 
 export const GameSelector = () => {
@@ -9,57 +11,14 @@ export const GameSelector = () => {
 
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-secondary px-4">
-      <div className="bg-primary w-full max-w-xl rounded-3xl p-8 space-y-2 items-start">
-        <h2 className="text-white text-2xl font-extrabold text-center">
-          Choose Your Game Mode
-        </h2>
-
-        <Link to="/game/classic-mode" >
-          <div className="bg-[url(/images/classic-button.png)"></div>
-        </Link>
-
-
-
-        <div id="classic-button-container" className="flex">
-          <GameModeButton
-            label="Classic"
-            onClick={() => navigate("/classic")}
-            icon="/icons/classic.png"
-            className={`${baseStyles} game-mode-button-classic`}
-          />
-        </div>
-
-        {/* Fila 2: dos botones iguales */}
-        <div className="flex gap-2">
-          <div className="flex-1">
-            <GameModeButton
-              label="Guess the Music"
-              onClick={() => navigate("/music")}
-              icon="/icons/music.png"
-              className={baseStyles}
-            />
-          </div>
-          <div className="flex-1">
-            <GameModeButton
-              label="Know the frame?"
-              onClick={() => navigate("/frame")}
-              icon="/icons/frame.png"
-              className={baseStyles}
-            />
-          </div>
-        </div>
-
-        {/* Fila 3: botón ancho pequeño */}
-        <div>
-          <GameModeButton
-            label="Who’s that character?"
-            onClick={() => navigate("/character")}
-            icon="/icons/guesswho.png"
-            className={baseStyles}
-          />
-        </div>
-      </div>
+    <div className="flex flex-col items-center justify-center bg-secondary px-4 gap-13">
+      <VideoPet />
+      <Link to="/game/classic-mode" >
+        <motion.img
+          src="/images/classic-button.png"
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: .5 }} />
+      </Link>
     </div>
   );
 };
