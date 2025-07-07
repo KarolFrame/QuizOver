7b8f15def79b40be0200e649df6e5d7884300dbe
Carolina <97622225+KarolFrame@users.noreply.github.com>
2025-07-07 12:38:24 +0000

    change visual ui

--- src/front/pages/Login.jsx
+++ src/front/pages/Login.jsx
@@ -1,12 +1,28 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import { LoginForm } from "../components/LoginForm.jsx";
+import { motion } from "motion/react";
+import { VideoPet } from "../components/VideoPet.jsx";
 
 export const Login = () => {
+	const [isIdle, setIsIdle] = useState(false);
 
-	return (
-		<div className="min-h-screen flex flex-col items-center justify-center text-center transform -translate-y-30">
-			<LoginForm />
+	return (<>
+		<div className=" d-flex flex-column gap-0 text-center">
+			<div className="flex justify-center items-center">
+				< div className="justify-center p-4 md:translate-y-50" >
+					<div className="flex flex-col items-center justify-center gap-0 md:flex-row md:items-center md:justify-center w-full max-w-screen-lg lg:max-w-screen-xl ">
+						<VideoPet onIdle={() => setIsIdle(true)} />
+						<motion.div
+							initial={{ opacity: 0, x: 500 }}
+							animate={{ opacity: 1, x: 0 }}
+							transition={{ duration: 1, delay: 2, ease: "easeOut" }}
+						>
+							<LoginForm />
+						</motion.div>
+					</div>
+				</div >
+			</div>
 		</div>
-	);
+	</>);
 }; 
